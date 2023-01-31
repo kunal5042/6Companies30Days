5f7a2001ef64097d16bbbcf3fb4deeed344d75f4
Kunal Wadhwa <kunalwadhwa.cs@gmail.com>
2023-01-31 11:18:08 +0530

    Updated

---
+++ [Amazon] Number of Ways to Reach a Position After Exactly k Steps.py
@@ -0,0 +1,26 @@
+# Question: https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
+# Medium
+
+
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        @cache
+        def memoization(position, count):
+            if count > k:
+                return 0
+            
+            if position == endPos and count == k:
+                return 1
+            
+            return memoization(position+1, count+1) + memoization(position-1, count+1)
+
+        return memoization(startPos, 0) % ((10 ** 9) + 7)
+
+
+# January 31, 2023
+
+'''
+
+# Kunal Wadhwa
+
+'''
